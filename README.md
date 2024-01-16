4974cc24d1347b97078f4a8b5b4a3ff18d69ac96
diy-pwa <rhildred@salesucation.com>
2024-01-16 15:54:57 -0500

    hopefully better idea

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # wp-pwa
 
+## bundler for wordpress site
+
+This is a vite plugin to create a static site from a wordpress site.
+
 [Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/diy-pwa/wp-pwa)
--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-// run `node index.js` in the terminal
+import { NodePHP }  from '@php-wasm/node';
+const php = await NodePHP.load('8.0');
+await php.useHostFilesystem();
+await php.writeFile("starter.php", `
+<?php
+$path = "test";
+if ($path != "/"){
+    $_SERVER["PATH_INFO"] = "/" . $path ."/";
+    mkdir('dist/' . $path, 0777, true);
+    $myfile = fopen("dist/". $path . "/index.html", "w") or die("Unable to open file!");
+}else{
+    $myfile = fopen("dist/index.html", "w") or die("Unable to open file!");
+}
+print_r(scandir("./"));
+print_r(file_get_contents("index.php"));
+ob_start();
+// Include the template file
+include("index.php");
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// End buffering and return its contents
+$output = ob_get_clean();
+$output = preg_replace("/(http|https):\/\/(localhost|127.0.0.1|playground\.wordpress\.net\/scope):\d+\.?\d*\/*/", "/", $output);
+fwrite($myfile, $output);
+fclose($myfile);		       
+`);
+const results = await php.run({
+    scriptPath: "starter.php",
+});
+
+console.log(results.text);
+
+process.exit();
