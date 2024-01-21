5363b02a484d33e9b2dc7a26b083af2e4087eab4
Rich Hildred <rhildred@wlu.ca>
2024-01-20 20:11:00 -0500

    creates output files

--- index.php
+++
@@ -1,1 +0,0 @@
-<?php require_once("required.php");
--- request.js
+++ request.js
@@ -1,21 +1,38 @@
-import { NodePHP }  from '@php-wasm/node';
-const requestHandler = {
-    documentRoot: new URL('./', import.meta.url).pathname,
-    absoluteUrl: "https://example.com"
-};
-const php = await NodePHP.load('8.0', {
-	requestHandler: requestHandler});
-await php.useHostFilesystem();
-//const results = await php.run({scriptPath: "index.php"});
-const data = {
-    url: `https://example.com/`,
-    headers: {},
-    method: 'GET',
-    files: {},
-    body: '',
-};
-const resp = await php.request(data);
+import { NodePHP } from '@php-wasm/node';
+import fs from 'fs';
 
-console.log(resp.text);
+class PHPLoader {
+    async load(sPath) {
+        if (!this.php){
+            const requestHandler = {
+                documentRoot: new URL('./wordpress', import.meta.url).pathname,
+                absoluteUrl: "https://example.com"
+            };
+            this.php = await NodePHP.load('8.0', {
+                requestHandler: requestHandler
+            });
+            await this.php.useHostFilesystem();
+            
+        }
+        const data = {
+            url: `https://example.com/${sPath}`,
+            headers: {},
+            method: 'GET',
+            files: {},
+            body: '',
+        };
+        return await this.php.request(data);
+    }
+}
 
-process.exit();
+const php = new PHPLoader();
+const paths = ["", "sample"];
+for(let path of paths){
+    const resp = await php.load(path);
+    if(!fs.existsSync(`dist/${path}`)){
+        await fs.promises.mkdir(`dist/${path}`, {recursive:true});
+    }
+    await fs.promises.writeFile(`dist/${path}/index.html`, resp.bytes);
+}
+
+process.exit(0);
