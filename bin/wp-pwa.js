eccb868c24291c0980d7a45b619fc91daf284290
Rich Hildred <rhildred@wlu.ca>
2024-01-27 14:54:43 -0500

    get tool working

--- bin/wp-pwa.js
+++ bin/wp-pwa.js
@@ -1,3 +1,19 @@
 #! /usr/bin/env node
 
-console.log("Rich was here");
+import PhpLoader from '../lib/index.js';
+
+const oLoader = new PhpLoader();
+switch(process.argv[2]){
+    case "build":
+        await oLoader.build(process.argv[3]);
+        process.exit();
+    case "preview":
+        await oLoader.preview(process.argv[3]);
+        break;
+    case "start":
+    case "dev":
+        await oLoader.dev();
+        break;
+    default:
+        console.log("wp-pwa (build|preview|dev|start) ?output_folder");
+}
