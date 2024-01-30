3350f7e73524780c564911de142745eb4f807417
Rich Hildred <rhildred@wlu.ca>
2024-01-30 12:06:49 -0500

    image upload works now as well

--- bin/wp-pwa.js
+++ bin/wp-pwa.js
@@ -1,19 +1,29 @@
 #! /usr/bin/env node
 
 import PhpLoader from '../lib/index.js';
+import path from "node:path";
 
+const sProg = path.basename(process.argv[1]);
+const sCommand = process.argv[2];
 const oLoader = new PhpLoader();
-switch(process.argv[2]){
+let app = null;
+switch(sCommand){
     case "build":
         await oLoader.build(process.argv[3]);
-        process.exit();
     case "preview":
-        await oLoader.preview(process.argv[3]);
+        app = await oLoader.preview(process.argv[3]);
         break;
     case "start":
     case "dev":
-        await oLoader.dev();
+        app = await oLoader.dev();
         break;
     default:
-        console.log("wp-pwa (build|preview|dev|start) ?output_folder");
+        console.log(`wp-pwa (build|preview|dev|start) ?output_folder`);
+}
+if(app){
+    const server = app.listen(8000, ()=>{
+        console.log(`\`${sProg} ${sCommand}\` is listening on port ${server.address().port}`);
+    })
+}else{
+    process.exit();
 }
